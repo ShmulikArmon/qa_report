c8bef68b215e00c339b59b9812110431b41ed21c
ShmulikArmon <shmulika@tabtale.com>
2014-08-17 02:28:50 +0300

    populate now works with the current data structure

--- populatedb.py
+++ populatedb.py
@@ -15,19 +15,27 @@ def URLRequest(url, params, method="GET"):
         return urllib2.Request(url + "?" + urllib.urlencode(params))
 
 
-def create_jira_connect(srv, cmp, tag, epc):
-    j = Jira.objects.get_or_create(service=srv, component_id=cmp, tag_id=tag, epic_link=epc)[0]
+def create_report(name, jql, testrail):
+    r = Report.objects.get_or_create(name=name, jira_jql=jql, testrail_plan_id=testrail)[0]
+    return r
+
+
+def create_jira_connector(report):
+    j = Jira.objects.get_or_create(report=report)[0]
     return j
 
 
-def create_bug(id, des, pr, st, fv, ass):
-    b = Bug.objects.get_or_create(bug_id=id, description=des, priority=pr, status=st,
+def create_bug(jc, id, des, pr, st, fv, ass):
+    b = Bug.objects.get_or_create(jira_connect=jc, bug_id=id, description=des, priority=pr, status=st,
                                   fix_version=fv, assignee=ass)[0]
     return b
 
 
 def populate():
-    result = jira.search_issues('component="PSDK Server"')
+    # result = jira.search_issues('component="PSDK Server"')
+    rep = create_report("Publishing Server", 'component="PSDK Server"', "1")
+    jir = create_jira_connector(rep)
+    result = jira.search_issues(jir.report.jira_jql)
     i = 0
     for item in result:
         id = result[i].key
@@ -38,7 +46,7 @@ def populate():
         if len(result[i].fields.fixVersions) != 0:
             fv = result[i].fields.fixVersions[0]
         ass = result[i].fields.assignee.displayName
-        create_bug(id, des, pr, st, fv, ass)
+        create_bug(jir, id, des, pr, st, fv, ass)
         i += 1
 
 
@@ -49,5 +57,5 @@ def populate_call():
 if __name__ == '__main__':
     print "Starting Population of DB..."
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qa_report_2.settings')
-    from report_view.models import Jira, Bug
+    from report_view.models import Jira, Bug, Report
     populate()
--- report_view/tables.py
+++ report_view/tables.py
@@ -2,7 +2,7 @@ from django_tables2 import tables
 from report_view.models import Bug
 
 class Report_Table(tables.Table):
-    bug_id = tables.columns.TemplateColumn('<a href="https://tabtale.atlassian.net/browse/{{ record.bug_id }}">{{ record.bug_id }}</a>')
+    bug_id = tables.columns.TemplateColumn('<a href="https://tabtale.atlassian.net/browse/{{ record.bug_id }}" target="_blank">{{ record.bug_id }}</a>')
     description = tables.columns.Column()
     priority = tables.columns.TemplateColumn('{% if record.priority == "Blocker" or record.priority == "Critical" %}<font color="red">{{ record.priority }}</font>{% else %}{{ record.priority }}{% endif %}')
     status = tables.columns.Column()
